e1f995cd6b3fd8a23b146c8e17ac55434fc2b1a2
Lumso <bavuttiluca@gmail.com>
2024-03-20 12:11:52 +0100

    Bounce projectile fixed

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,37 +4,29 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    public LayerMask collisionMask;
-
-    public float speed = 15;
-    RaycastHit hit;
-    Ray ray;
-
-    //https://www.youtube.com/watch?v=u_p5H0wEN8Y
-    void Start()
-    {
-        
-    }
-
+    public float speed;
+    public LayerMask layerMask;
+    public float raycastDistance = 10f;
 
     void Update()
     {
         Vector2 movement = Vector2.right * speed * Time.deltaTime;
         transform.Translate(movement);
 
-        RaycastHit hit;
-        Ray ray = new Ray(transform.position, movement.normalized);
+        Ray ray = new Ray(transform.position, transform.right);
+        // Shoot a raycast from the GameObject's position in the forward direction
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, raycastDistance, layerMask);
 
-        if (Physics.Raycast(ray, out hit, movement.magnitude + .5f, collisionMask))
+        //Debug.DrawRay(transform.position, transform.right * raycastDistance, Color.green);
+
+        // Check if the raycast hit a collider on the specified layer mask
+        if (hit.collider != null)
         {
-            Debug.Log("Collision detected with: " + hit.transform.gameObject.name);
-
-            //        Vector3 reflectDirection = Vector2.Reflect(ray.direction, hit.normal);
-            //        float rot = 90 - Mathf.Atan2(reflectDirection.z, reflectDirection.x) * Mathf.Rad2Deg;
-            //        transform.eulerAngles = new Vector2(0, rot);
+            ////TO DO: fixare la rotazione
+            Vector3 reflectDir = Vector3.Reflect(ray.direction, hit.normal);
+            Debug.Log("reflectDir " + reflectDir);
+            float rot = Mathf.Atan2(reflectDir.y, reflectDir.x) * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0, 0, rot);
         }
-        Debug.DrawRay(transform.position, movement.normalized * (movement.magnitude + .5f), Color.green);
-
     }
-
 }
